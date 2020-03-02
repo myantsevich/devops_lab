89f4ebcf66c24ccf13c5e013c71a347618ff37e0
mary <40528235+myantsevich@users.noreply.github.com>
2020-03-03 00:29:40 +0300

    fixes for task2

--- task1.py
+++ task1.py
@@ -1,4 +1,3 @@
 # enter a year without spaces and letters
 year = int(input())
-print('TRUE') if year % 4 == 0 and (year % 100 != 0 or year % 100 and year % 400)
-else print('FALSE')
+print('TRUE' if year % 4 == 0 and (year % 100 != 0 or year % 100 and year % 400) else 'FALSE')
