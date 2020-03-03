fa339378539a076e25b335b4293e6b83222632bb
mary <40528235+myantsevich@users.noreply.github.com>
2020-03-03 23:34:58 +0300

    fixes

--- task1.py
+++ task1.py
@@ -1,3 +1,3 @@
 # enter a year without spaces and letters
 year = int(input())
-print('TRUE' if year % 4 == 0 and (year % 100 != 0 or year % 100 and year % 400) else 'FALSE')
+print('True' if year % 4 == 0 and (year % 100 != 0 or year % 100 and year % 400) else 'False')
--- task5.py
+++ task5.py
@@ -1,8 +1,10 @@
-# enter 2 numbers, every from new line
-left = int(input())
-right = int(input())
+# enter 2 numbers to one line
+border = input().split()
+left = int(border[0])
+right = int(border[1])
 result = [
     x for x in range(left, right + 1)
     if '0' not in str(x) and all((x % int(char) == 0 for char in str(x)))
 ]
 print(result)
+
