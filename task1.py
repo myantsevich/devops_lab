03374179b8275a8e0db7deb2566b58273c36e211
marysh <mary@mary.com>
2020-03-05 17:11:14 +0300

    remove unnecessary files

--- task1.py
+++ task1.py
@@ -1,3 +1,3 @@
 # enter a year without spaces and letters
 year = int(input())
-print('True' if year % 4 == 0 and (year % 100 != 0 or year % 100 and year % 400) else 'False')
+print(year % 4 == 0 and (year % 100 != 0 or year % 100 and year % 400))
